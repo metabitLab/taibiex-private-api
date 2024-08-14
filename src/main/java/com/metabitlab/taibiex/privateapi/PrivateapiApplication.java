75735df5a72d1c271462c63ccbb373a171b7fa5b
nix <nix@mecx.com>
2024-08-14 17:12:09 +0800

    initial commit

---
+++ src/main/java/com/metabitlab/taibiex/privateapi/PrivateapiApplication.java
@@ -0,0 +1,13 @@
+package com.metabitlab.taibiex.privateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metabitlab/taibiex/privateapi/PrivateapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metabitlab.taibiex.privateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
