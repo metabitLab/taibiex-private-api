2f535891c70cae119ee997b8cd2965753837c3b5
nix <nix@mecx.com>
2024-08-14 19:27:43 +0800

    refactor: add subgraphs schema

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Taibiex Private API
+
+## Introduction
+
+## Contribution
+
+### Development Environment
+
+- JDK 17
+- Maven 3.8.6
--- src/main/java/com/metabitlab/taibiex/privateapi/fetcher/TokenDataFetcher.java
+++ src/main/java/com/metabitlab/taibiex/privateapi/fetcher/TokenDataFetcher.java
@@ -1,25 +1,44 @@
 package com.metabitlab.taibiex.privateapi.fetcher;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.Token;
+import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolsGraphQLQuery;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
+import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 
+/**
+ * This class is responsible for fetching token data.
+ * 
+ * curl -X POST http://localhost:8803/api/graphql/query \
+ * -H "Content-Type: application/json" \
+ * -d '{"query": "query Wanda($chain: Chain!, $address: String) { token(chain: $chain, address: $address) { name } }", "operationName": "Wanda", "variables": { "chain": "ETHEREUM", "address": "321" }}'
+ * 
+ * @author Nix
+ */
 @DgsComponent
 public class TokenDataFetcher {
-  /**
-   * This class is responsible for fetching token data.
-   * 
-   * curl -X POST http://localhost:8803/api/graphql/query \
-   * -H "Content-Type: application/json" \
-   * -d '{"query": "query Wanda($chain: Chain!, $address: String) { token(chain: $chain, address: $address) { name } }", "operationName": "Wanda", "variables": { "chain": "ETHEREUM", "address": "321" }}'
-   * 
-   * @author nix
-   */
+
+  @Value("${app.subgraphs.endpoint}")
+  private String subgraphsEndpoint;
+
   @DgsQuery
   public Token token(@InputArgument String chain, @InputArgument String address) {
     System.out.println("chain: " + chain);
     System.out.println("address: " + address);
+
+    GraphQLQueryRequest request = new GraphQLQueryRequest(
+      new PoolsGraphQLQuery.Builder()
+        .skip(0)
+        .first(100)
+        .build()
+    );
+
+    String requestBody = request.serialize();
+    System.out.println(subgraphsEndpoint);
+    System.out.println(requestBody);
 
     return new Token() {
       {
