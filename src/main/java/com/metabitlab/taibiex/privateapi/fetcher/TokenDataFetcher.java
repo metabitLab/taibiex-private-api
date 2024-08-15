9aa58df9146ec3910c9a4ebe46d6b22fef4a77fc
nix <nix@mecx.com>
2024-08-15 15:28:18 +0800

    feat: 实现 TokenSpotPrice query 查询 ETH 现价的功能
    其他字段值需等待数据存储功能

--- src/main/java/com/metabitlab/taibiex/privateapi/fetcher/TokenDataFetcher.java
+++ src/main/java/com/metabitlab/taibiex/privateapi/fetcher/TokenDataFetcher.java
@@ -1,18 +1,29 @@
 package com.metabitlab.taibiex.privateapi.fetcher;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.alibaba.fastjson.JSON;
+import com.jayway.jsonpath.TypeRef;
 import com.metabitlab.taibiex.privateapi.configuration.SubgraphsClient;
+import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.Amount;
 import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.Chain;
+import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.Currency;
 import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.Token;
+import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.TokenProject;
+import com.metabitlab.taibiex.privateapi.graphqlapi.codegen.types.TokenProjectMarket;
 import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolsGraphQLQuery;
 import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolsProjectionRoot;
+import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.types.Pool;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 import com.netflix.graphql.dgs.client.GraphQLResponse;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
+
+import graphql.com.google.common.base.Objects;
 
 /**
  * This class is responsible for fetching token data.
@@ -31,29 +42,51 @@ public class TokenDataFetcher {
 
   @DgsQuery
   public Token token(@InputArgument String chain, @InputArgument String address) {
-    System.out.println("chain: " + chain);
-    System.out.println("address: " + address);
-
-    PoolsGraphQLQuery pools = PoolsGraphQLQuery.newRequest()
+    PoolsGraphQLQuery poolsQuery = PoolsGraphQLQuery.newRequest()
       .skip(0)
       .first(100)
-      .queryName("TokenSpotPrice")
+      .queryName("TokenDataFetcher")
       .build();
 
     PoolsProjectionRoot<?, ?> poolsProjection = new PoolsProjectionRoot<>()
+      .token0Price()
       .id();
 
-    GraphQLQueryRequest request = new GraphQLQueryRequest(pools, poolsProjection);
-
+    GraphQLQueryRequest request = new GraphQLQueryRequest(poolsQuery, poolsProjection);
     GraphQLResponse response = subgraphsClient.build().executeQuery(request.serialize());
 
-    System.out.println(request.serialize());
-    System.out.println(JSON.toJSONString(response.extractValue("pools")));
+    List<Pool> pools = response.extractValueAsObject("pools", new TypeRef<List<Pool>>() {});
+    Optional<Pool> target = pools.stream()
+      .filter(item -> Objects.equal(item.getId(), "0x6cec4df7ad64d5d06860a397c17edebc5f311ae3"))
+      .findFirst();
+    double price = target.orElse(null).getToken0Price().doubleValue();
 
     return new Token() {
       {
-        setAddress(address);
+        setId("123");
         setChain(Chain.valueOf(chain));
+        setAddress(address);
+        setName("Ethereum");
+        setSymbol("ETH");
+        setProject(new TokenProject() {
+          {
+            setId("123");
+            setMarkets(Arrays.asList(
+              new TokenProjectMarket() {
+                {
+                  setId("123");
+                  setPrice(new Amount() {
+                    {
+                      setId("123");
+                      setCurrency(Currency.USD);
+                      setValue(price);
+                    }
+                  });
+                }
+              }
+            ));
+          }
+        });
       }
     };
   }
