d24d705d8c57e86a8b6ad6a145b7028abe2ee65c
milo9301 <milo199301@outlook.com>
2024-08-26 15:43:55 +0800

    feat：fix pools subgraph fetcher

--- src/main/java/com/metabitlab/taibiex/privateapi/subgraphfetcher/PoolsSubgraphFetcher.java
+++ src/main/java/com/metabitlab/taibiex/privateapi/subgraphfetcher/PoolsSubgraphFetcher.java
@@ -2,8 +2,7 @@ package com.metabitlab.taibiex.privateapi.subgraphfetcher;
 
 import java.util.List;
 
-import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolGraphQLQuery;
-import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolProjectionRoot;
+import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.*;
 import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.types.OrderDirection;
 import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.types.Pool_filter;
 import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.types.Pool_orderBy;
@@ -12,8 +11,6 @@ import org.springframework.stereotype.Service;
 
 import com.jayway.jsonpath.TypeRef;
 import com.metabitlab.taibiex.privateapi.configuration.SubgraphsClient;
-import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolsGraphQLQuery;
-import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.client.PoolsProjectionRoot;
 import com.metabitlab.taibiex.privateapi.subgraphsclient.codegen.types.Pool;
 import com.netflix.graphql.dgs.client.GraphQLResponse;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
@@ -60,6 +57,14 @@ public class PoolsSubgraphFetcher {
                 .totalValueLockedUSD()
                 .totalValueLockedUSDUntracked()
                 .feesUSD();
+
+        poolProjection.token0().id().decimals().name().symbol().totalSupply().derivedETH()
+                .totalValueLocked().totalValueLockedUSD().totalValueLockedUSDUntracked()
+                .txCount().untrackedVolumeUSD().volume().volumeUSD().poolCount();
+
+        poolProjection.token1().id().decimals().name().symbol().totalSupply().derivedETH()
+                .totalValueLocked().totalValueLockedUSD().totalValueLockedUSDUntracked()
+                .txCount().untrackedVolumeUSD().volume().volumeUSD().poolCount();
 
         GraphQLQueryRequest request = new GraphQLQueryRequest(poolsQuery, poolProjection);
         GraphQLResponse response = subgraphsClient.build().executeQuery(request.serialize());
