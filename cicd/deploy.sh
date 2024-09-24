46a2a419fa4f30c75012428f4b585bfaa8d1cf63
nix <nix@mecx.com>
2024-09-24 16:25:08 +0800

    chore: 升级deploy.sh脚本

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -21,7 +21,7 @@ mvn clean -f ./pom.xml
 mvn package -f ./pom.xml -DskipTests
 
 ## 获取最新的制品的名称
-artifact_name="$(ls -t ./target/*.jar | head -n 1 | awk -F '/' '{print $3}')"
+artifact_name="$(ls -t ./target/*-2.0.0.jar | head -n 1 | awk -F '/' '{print $3}')"
 
 echo "已构建的制品名称：$artifact_name, 是否继续上传制品并部署? (Y/[N])"
 
@@ -40,9 +40,9 @@ scp -i taibiex-gateway.pem ./target/$artifact_name ubuntu@ec2-3-84-205-22.comput
 ## - 启动服务
 ssh -i taibiex-gateway.pem ubuntu@ec2-3-84-205-22.compute-1.amazonaws.com << remote
 sudo su -
-mv /tmp/$artifact_name /home/taibiex-private-api
-cd /home/taibiex-private-api
-ps -aux | grep java | grep -v grep | awk '{print \$2}' | xargs -I _ kill -9 _
+mv /tmp/$artifact_name /home/taibiex-private-api-v2
+cd /home/taibiex-private-api-v2
+ps -aux | grep $artifact_name | grep -v grep | awk '{print \$2}' | xargs -I _ kill -9 _
 nohup java -jar $artifact_name > nohup.out &
 exit
 remote
